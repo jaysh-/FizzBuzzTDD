16a65b8c97dc9358125ff61586fd3f0e0c4b9ea5
Josh Spires <jspires@paraport.com>
2017-07-18 11:40:25 -0700

    Added loop test
    Loop tests builds the results of fizzbuzz from 1 to 31 inclusively.

--- FizzBuzzTDD/FizzBuzz.Tests/FizzBuzzinTests.cs
+++ FizzBuzzTDD/FizzBuzz.Tests/FizzBuzzinTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
 using static FizzBuzz.FizzBuzzin;
 
@@ -30,6 +31,21 @@ namespace FizzBuzz.Tests
 		public void OneFizzBuzz()
 		{
 			GetNthFizzBuzz(15).ShouldBe("Fizz Buzz");
+		}
+
+		[TestMethod]
+		public void NaturalsFrom1To31Inclusive()
+		{
+			var expected = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17," +
+			               " Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31";
+			var actual = "";
+			foreach (int i in Enumerable.Range(1, 30))
+			{
+				actual += (GetNthFizzBuzz(i) + ", ");
+			}
+			//Fence Post
+			actual += GetNthFizzBuzz(31);
+			actual.ShouldBe(expected);
 		}
 	}
 }
